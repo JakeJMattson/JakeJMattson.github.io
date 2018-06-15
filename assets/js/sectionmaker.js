df54d5c11ef547662c50ed2257c7f10036419d08
Jake <JakeJMattson@gmail.com>
2018-06-14 23:40:30 -0500

    Added star count
    Added the number of GitHub stars acquired by each repository next to their corresponding button.

--- assets/js/sectionmaker.js
+++ assets/js/sectionmaker.js
@@ -1,5 +1,17 @@
 function makeSection(name)
 {
+	//Opening tag
+	document.write("<div>");
+	
+	//Create a button linking to the GitHub Pages site
 	var site = "https://mattson543.github.io/" + name;
 	document.write("<a href=" + site + " class=\"button\">" + name + "</a>");
+	
+	//Display the repository's star count
+	var starImg = "https://img.shields.io/github/stars/mattson543/" + name + ".svg?style=social&amp";
+	var starCount = "https://GitHub.com/mattson543/" + name + "/stargazers/";
+	document.write("<a href= " + starCount + "><img src = " + starImg + "alt=\"GitHub stars\"></a>");
+	
+	//Closing tag
+	document.write("</div>");
 }
