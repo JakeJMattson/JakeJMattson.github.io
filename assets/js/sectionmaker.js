f4c950288dfb5d53b0c3a703d6d1107a86122817
Jake <JakeJMattson@gmail.com>
2018-07-06 11:29:17 -0500

    Complete redesign

--- assets/js/sectionmaker.js
+++ assets/js/sectionmaker.js
@@ -1,34 +1,22 @@
-function createProject(name)
+function createProjectSection(name, description)
 {
-	//Opening tag
-	document.write("<div>");
-	
-	//Create a button linking to the GitHub Pages site
-	var site = "https://mattson543.github.io/" + name;
-	document.write("<a href=" + site + " class=\"button\">" + name + "</a>");
-	
-	//Display the repository's star count
-	var starImg = "https://img.shields.io/github/stars/mattson543/" + name + ".svg?style=social&amp";
-	var starCount = "https://GitHub.com/mattson543/" + name + "/stargazers/";
-	document.write("<a href= " + starCount + "><img src = " + starImg + "alt=\"GitHub stars\"></a>");
-	
-	//Closing tag
-	document.write("</div>");
-}
+	var html = [
+	    '<div class="col-12 col-md-6 col-lg-4 pt-5 pb-3">',
+	        '<div class="card mx-auto" style="max-width:400px">',
+	        '<div class="mb-0" style="height:290px;overflow:hidden;background-color:#e5e5e5">',
+	        	'<img src="assets/images/' + name + '.PNG" alt="Demo" class="img-fluid rounded-top" style="vertical-align:middle;min-height:100%"/>',
+	        '</div>',
+        		'<div class="card-body" style="font-family:Oswald;font-weight:600;color:#3a4145">',
+        			'<h5 class="card-title" >' + name + '</h5>',
+        			'<p class="card-text">' + description + '</p>',
+        			'<div class="row">',
+        				'<div class="col-6"><a class="btn btn-outline-secondary" target="_blank" href="https://mattson543.github.io/' + name + '/">View Project</a></div>',
+        				'<div class="col-6"><a class="btn btn-outline-secondary" target="_blank" href="https://github.com/mattson543/' + name + '"><i class="fab fa-github"></i> GitHub</a></div>',
+	        		'</div>',
+	        	'</div>',
+	        '</div>',
+	    '</div>'
+	].join("\n");
 
-function createIcon(href, name, extension = ".png")
-{
-	//Construct arguments
-	var title = " title = " + addQuotes(name);
-	var alt = " alt = " + addQuotes(name);
-	var src = " src = \"./assets/images/" + name + extension + "\"";
-	var size = " height = \"40\" width = \"40\"";
-	
-	//Write arguments
-	document.write("<a href = " + addQuotes(href) + "><img " + title + alt + src + size + "/></a>");
-}
-
-function addQuotes(string)
-{
-	return "\"" + string + "\"";
+	document.write(html);
 }
